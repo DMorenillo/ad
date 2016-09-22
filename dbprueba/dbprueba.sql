37c8e9456bd5177a650e63e3b619a96f05282598
DMorenillo <guulas@gmail.com>
2016-09-22 09:26:10 +0200

    actualizada la bases de datos

--- dbprueba/dbprueba.sql
+++ dbprueba/dbprueba.sql
@@ -5,3 +5,17 @@ CREATE TABLE categoria (
 insert into categoria (nombre) values ('categoria 1');
 insert into categoria (nombre) values ('categoria 2');
 insert into categoria (nombre) values ('categoria 3');
+
+create table articulo (
+id bigint auto_increment primary key,
+nombre varchar (50) not null unique,
+precio decimal (10,2),
+categoria bigint
+);
+
+alter table articulo add foreign key (categoria) references categoria (id);
+
+insert into articulo (nombre, precio, categoria) values ('articulo 1', 1,1);
+insert into articulo (nombre, precio, categoria) values ('articulo 2', 2,2);
+insert into articulo (nombre, precio) values ('articulo 3', 3);
+insert into articulo (nombre) values ('articulo 4');
