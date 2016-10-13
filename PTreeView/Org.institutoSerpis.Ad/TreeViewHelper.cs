de397da4ae2a96b69de47f2c86b1ba1c098f6510
DMorenillo <guulas@gmail.com>
2016-10-13 08:26:12 +0200

    Actualizado PTreeview el martes 11/10

--- PTreeView/Org.institutoSerpis.Ad/TreeViewHelper.cs
+++ PTreeView/Org.institutoSerpis.Ad/TreeViewHelper.cs
@@ -1,5 +1,8 @@
 using Gtk;
 using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Reflection;
 
 
 namespace Org.InstitutoSerpis.Ad
@@ -16,10 +19,55 @@ namespace Org.InstitutoSerpis.Ad
 						object value= tree_model.GetValue(iter, column);
 						cellRendererText.Text= value.ToString();
 						Console.WriteLine("index=(0) column=(1)", index, column);
+					}
+				);
+			}
+		}
+		public static void AppendColumns (TreeView treeView, Type type){
+			PropertyInfo[] propertyInfos = type.GetProperties();
+			List<string> propertyNames = new List <string>();
+			foreach (PropertyInfo propertyInfo in propertyInfos)
+				propertyNames.Add (propertyInfo.Name);
+			AppendColumns (treeView, propertyNames.ToArray());
+		}
+
+		private static void appendColumns (TreeView treeView, IList list){
+			if (treeView.Columns !=0)
+			Type listType = list.GetType ();
+			//Console.WriteLine ("listType={0}", listType);
+			Type[] elementType = listType.GetGenericArguments ();
+			//Console.WriteLine ("elementType={0}", genericArgument);
+			PropertyInfo[] propertyInfos = elementType.GetProperties ();
+			int columnIndex = 0;
+			foreach (PropertyInfo propertyInfo in propertyInfos) {
+				string columnName = propertyInfo.Name;
+				treeView.AppendColumn (columnName, new CellRendererText (),
+				                       delegate(TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter) {
+					object Item = tree_model.GetValue (iter, 0);
+					object value = propertyInfo.GetValue (Item, null);
+					Console.WriteLine ("item.GetType()={0}", Item.GetType ());
+					CellRendererText CellRendererText = (CellRendererText)cell;
+					CellRendererText.Text = value == null ? "": value.ToString();
 				}
 				);
 			}
 		}
+		private static void appendValues (TreeView treeView, IList list){
+			ListStore listStore = new ListStore(typeof(long));
+			treeView.Model= listStore;
+			foreach(object item in list)
+				listStore.AppendValues(item);
+		}
+
+		public static void Fill(TreeView treeView, IList list){
+			appendColumns (treeView, list);
+			appendValues (treeView, list);
+			// Aqui añadimos Datos
+
+		}
+	}
+
+	
 	}
 }
 
--- PTreeView/PTreeView/MainWindow.cs
+++ PTreeView/PTreeView/MainWindow.cs
@@ -1,20 +1,39 @@
 using System;
 using Gtk;
 using Org.InstitutoSerpis.Ad;
+using System.Collections.Generic;
+using System.Collections;
 
 public partial class MainWindow: Gtk.Window
 {	
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
-		string[] columnNames = { "id", "nombre", "precio" };
-		TreeViewHelper.AppendColumns(treeView, columnNames);
+
+		IList list= new List<Articulo> ();
+		//Articulo articulo = new Articulo ();
+		//articulo.Id = 1L;
+		//articulo.Nombre = "articulo 1";
+		//articulo.Precio = 1.5m;
+//		list.Add (new Articulo (1L, " articulo 1", 1.5m));
+//		list.Add (new Articulo (1L, " articulo 2", 2.5m));
+//		list.Add (new Articulo (1L, " articulo 3", 3.5m));
+		list.Add (new Categoria (1L, " categoria 1", 1.5m));
+		list.Add (new Categoria (1L, " categoria 2", 2.5m));
+		list.Add (new Categoria (1L, " categoria 3", 3.5m));
+
+		//string[] columnNames = { "id", "nombre", "precio" };
+
+		TreeViewHelper.Fill (treeView, list);
 
 
-		ListStore listStore = new ListStore (typeof(long), typeof(string),typeof(decimal));
-		treeView.Model = listStore;
-		listStore.AppendValues (1L, "categoria 1",1.5m);
-		listStore.AppendValues (2L, "categoria 2",2.5m);
+		TreeViewHelper.AppendColumns(treeView, list);
+
+
+//		ListStore listStore = new ListStore (typeof(long), typeof(string),typeof(decimal));
+//		treeView.Model = listStore;
+//		listStore.AppendValues (1L, "categoria 1",1.5m);
+//		listStore.AppendValues (2L, "categoria 2",2.5m);
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
@@ -22,4 +41,19 @@ public partial class MainWindow: Gtk.Window
 		Application.Quit ();
 		a.RetVal = true;
 	}
+}
+public class Categoria {
+	public long Id { get; set;}
+	public string Nombre { get; set;}
+}
+public class Articulo{
+			public Articulo (long id, string nombre, decimal precio){
+				id = id;
+				nombre = nombre;
+				precio = precio;
+			}
+
+	public long Id { get; set;}
+	public string Nombre { get; set;}
+	public decimal Precio { get; set;}
 }
